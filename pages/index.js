7da21302a622cff461b55f75ce239cf50196a3d9
Dan <dan.tran301@gmail.com>
2021-07-20 13:39:29 -0700

    Animate navbar
    Navbar animation on open and close. Add X icon to close.

--- pages/index.js
+++ pages/index.js
@@ -4,52 +4,21 @@ import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import Hero from "../components/layout/Hero";
 import Projects from "../components/layout/Projects";
+import Box from "../components/layout/Box";
 
 function Home() {
-  const Box1 = () => {
-    const controls = useAnimation();
-    const { ref, inView } = useInView();
-
-    useEffect(() => {
-      if (inView) {
-        controls.start("visible");
-      }
-      if (!inView) {
-        controls.start("hidden");
-      }
-    }, [controls, inView]);
-    const boxVariants = {
-      hidden: { scale: 0 },
-      visible: {
-        scale: 1,
-        transition: {
-          duration: 0.5,
-        },
-      },
-    };
-
-    return (
-      <motion.div
-        animate={controls}
-        ref={ref}
-        variants={boxVariants}
-        className={styles.box}
-      ></motion.div>
-    );
-  };
-
   return (
     <>
       <div>
         <Hero />
         <Projects />
-        <Box1 />
-        <Box1 />
-        <Box1 />
-        <Box1 />
-        <Box1 />
-        <Box1 />
-        <Box1 />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
       </div>
     </>
   );
