1ecf327d138309594bd23648964654e4ad74c6dc
Dan <dan.tran301@gmail.com>
2021-09-18 17:16:48 -0700

    Initialize Google Tag Manager
    Install react-gtm-module to handle google tag manager setup. Hook up
    google tag manager to run with Google Analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
 import { NavContextProvider } from "../context/NavContext";
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
+  const tagManagerArgs = {
+    gtmId: "GTM-KDVRWHN",
+    events: {
+      sendUserInfo: "userInfo",
+    },
+  };
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <NavContextProvider>
       <Layout>
