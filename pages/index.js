09f82ed3a715c732b44df9ef1aaf95896d6f81a6
Dan Tran <65366053+dtran5@users.noreply.github.com>
2021-09-28 15:45:17 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ function Home() {
     <>
       <Head>
         <title>Dan Tran</title>
-        <meta content="/siteImages/thumbnail.png" />
+        <meta property="og:image" content="/siteImages/thumbnail.png" />
       </Head>
       <div className="w-11/12 sm:w-10/12 md:w-10/12 mx-auto overflow-x-hidden">
         <Hero />
